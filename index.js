de2e5f0e77eb8dda66ae867d77269dd1de0df6ae
Drew Thompson <drewallynthompson@gmail.com>
2021-01-03 07:21:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ function unleashDog(dogName, dogBreed){
 let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
 
 function exerciseDog(dogName, dogBreed){
-  for (i = 0; i < routine.legth)
+  for (i = 0; i < routine.legth, i++){
+    i()
+  }
 }
